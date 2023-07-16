a47982a4c0ffba803d4067eb8b178109acb2d9d2
Byeongchan Park <pbc1017@kaist.ac.kr>
2023-07-16 22:14:55 +0900

    finish home

--- front/src/pages/Map/Map.tsx
+++ front/src/pages/Map/Map.tsx
@@ -3,24 +3,20 @@ import { Card } from "../../components/Card";
 import { Header } from "../../components/Header";
 import { SearchBar } from "../../components/SearchBar";
 import {KakaoMap} from "../../components/KakaoMap"; 
+import searchSvg from "assets/images/search.svg"
+import logoSvg from "assets/images/logo.svg"
 import "./style.css";
 
 export const Map = (): JSX.Element => {
   return (
     <div className="map">
       <div className="div-2">
+        <Header className="header-instance" loginLogo={logoSvg} />
         <div className="overlap">
+          <SearchBar iconSearch={searchSvg}/>
           <KakaoMap/>
-          <SearchBar
-            iconSearch="/img/icon-search.svg"
-          />
           <div className="frame-8">
-            <Card
-              anonymousUser="/img/anonymous-user-6.svg"
-              className="card-instance"
-              text = ""
-              heart="/img/heart-6.svg"
-            />
+            <Card anonymousUser="/img/anonymous-user-6.svg" className="card-instance" text = "" heart="/img/heart-6.svg" />
             <Card anonymousUser="/img/anonymous-user-5.svg" className="card-instance" text = "" heart="/img/heart-5.svg" />
             <Card anonymousUser="/img/anonymous-user-4.svg" className="card-instance" text = "" heart="/img/heart-4.svg" />
             <Card anonymousUser="/img/anonymous-user-3.svg" className="card-instance" text = "" heart="/img/heart-3.svg" />
@@ -28,7 +24,6 @@ export const Map = (): JSX.Element => {
             <Card anonymousUser="/img/anonymous-user-1.svg" className="card-instance" text = "" heart="/img/heart-1.svg" />
           </div>
         </div>
-        <Header className="header-instance" loginLogo="/img/login-logo.svg" />
       </div>
     </div>
   );
