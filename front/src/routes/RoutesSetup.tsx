e88e2d757e4b6db17b7f1dffa13f00398cb3d1b3
Byeongchan Park <pbc1017@kaist.ac.kr>
2023-07-18 22:01:12 +0900

    detail design

--- front/src/routes/RoutesSetup.tsx
+++ front/src/routes/RoutesSetup.tsx
@@ -2,6 +2,7 @@ import {Routes, Route} from 'react-router-dom'
 import {Login} from '../pages/Login/'
 import Home from './Home'
 import {Map} from '../pages/Map/Map'
+import { Detail } from 'pages/Detail'
 
 
 export default function RouteSetup() {
@@ -10,7 +11,7 @@ export default function RouteSetup() {
             <Route path= "/" element = {<Home/>}/>
             <Route path= "/login" element = {<Login/>}/>
             <Route path= "/map" element = {<Map addressProp='' />}/>
-     
+            <Route path= "/detail" element = {<Detail/>}/>
         </Routes>
     )
 }
